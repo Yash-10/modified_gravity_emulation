85749a406dc04655f0c5f2fd2c11b71d502a70a6
Yash Gondhalekar <yashgondhalekar567@gmail.com>
2022-08-27 13:39:50 +0530

    Update util.py

--- pytorch-CycleGAN-and-pix2pix/util/util.py
+++ pytorch-CycleGAN-and-pix2pix/util/util.py
@@ -4,9 +4,22 @@ import torch
 import numpy as np
 from PIL import Image
 import os
+import torch
 
+def untransform(img, arr_min, arr_max):
+    """
+    arr_max and arr_min are max and min values of the original image before customNormalized but after log10 transform.
+    """
+    # First unnormalize
+    scaled = (img + 1) / 2
+    scaled = scaled.astype('f')
+    arr_range = arr_max - arr_min
+    arr = scaled * float(arr_range) + arr_min
+    # Then unlog
+    unlogged = 10 ** arr
+    return unlogged
 
-def tensor2im(input_image, imtype=np.uint8):
+def tensor2im(input_image, imtype=np.float32):
     """"Converts a Tensor array into a numpy image array.
 
     Parameters:
@@ -19,11 +32,10 @@ def tensor2im(input_image, imtype=np.uint8):
         else:
             return input_image
         image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
-        if image_numpy.shape[0] == 1:  # grayscale to RGB
-            image_numpy = np.tile(image_numpy, (3, 1, 1))
-        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
     else:  # if it is a numpy array, do nothing
         image_numpy = input_image
+    # image_numpy = untransform(image_numpy)
+    ### TODO: Currently, we cannot do untransform since we do not have a way to store original min and max values, so unnormalization cannot be done.
     return image_numpy.astype(imtype)
 
 
@@ -46,7 +58,7 @@ def diagnose_network(net, name='network'):
     print(mean)
 
 
-def save_image(image_numpy, image_path, aspect_ratio=1.0):
+def save_image(image_numpy, image_path):
     """Save a numpy image to the disk
 
     Parameters:
@@ -54,14 +66,7 @@ def save_image(image_numpy, image_path, aspect_ratio=1.0):
         image_path (str)          -- the path of the image
     """
 
-    image_pil = Image.fromarray(image_numpy)
-    h, w, _ = image_numpy.shape
-
-    if aspect_ratio > 1.0:
-        image_pil = image_pil.resize((h, int(w * aspect_ratio)), Image.BICUBIC)
-    if aspect_ratio < 1.0:
-        image_pil = image_pil.resize((int(h / aspect_ratio), w), Image.BICUBIC)
-    image_pil.save(image_path)
+    np.save(image_path, image_numpy)
 
 
 def print_numpy(x, val=True, shp=False):
