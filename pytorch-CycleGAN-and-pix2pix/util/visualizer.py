b7b1a2e9f8ebb927190a778ed656662eb9ddffbc
Yash Gondhalekar <yashgondhalekar567@gmail.com>
2022-08-28 11:18:13 +0530

    Update visualizer.py

--- pytorch-CycleGAN-and-pix2pix/util/visualizer.py
+++ pytorch-CycleGAN-and-pix2pix/util/visualizer.py
@@ -39,7 +39,7 @@ def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, use_w
     ims_dict = {}
     for label, im_data in visuals.items():
         im = util.tensor2im(im_data)
-        image_name = '%s_%s.png' % (name, label)
+        image_name = '%s_%s.npy' % (name, label)
         save_path = os.path.join(image_dir, image_name)
         util.save_image(im, save_path)
         ims.append(image_name)
