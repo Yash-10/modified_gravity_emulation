e80c67b96e92b17688b7b3fd565393e72adb3c35
Yash Gondhalekar <yashgondhalekar567@gmail.com>
2022-09-04 12:18:12 +0530

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -11,6 +11,20 @@ def unison_shuffled_copies(a, b):
     p = np.random.permutation(len(a))
     return a[p], b[p]
 
+def get_slice(arr_3d, i):
+    _l = arr_3d[i].split('/')[2]
+    a = gzip.GzipFile(_l.split('gz')[0]+'gz', 'r')
+    gr_3d = np.load(a)
+    print(_l)
+    if _l[6] == 'z':
+        arr_slice = gr_3d[:, :, int(_l.split('_')[7])]
+    elif _l[1] == 'y':
+        arr_slice = gr_3d[:, int(_l.split('_')[7]), :]
+    elif _l[1] == 'x':
+        arr_slice = gr_3d[int(_l.split('_')[7]), :, :]
+
+    return arr_slice
+
 if __name__ == "__main__":
     # Set seed for reproducibility
     np.random.seed(42)
@@ -33,79 +47,76 @@ if __name__ == "__main__":
 
     Ng = 512  # Grid size.
     assert len(glob.glob(f'{BASE_PATH}/example_GR_*_*den.npy.gz')) == len(glob.glob(f'{BASE_PATH}/example_F4n1_*_*den.npy.gz'))
-    total_slices = Ng * 3 * len(glob.glob(f'{BASE_PATH}/example_GR_*_*den.npy.gz'))  # These many slices for either GR or F4n1.
-
-    gr_all = np.empty((total_slices, Ng, Ng))
-    fr_all = np.empty((total_slices, Ng, Ng))
+    total_slices = Ng * 3 * len(glob.glob(f'{BASE_PATH}/example_GR_*_*_0157_den.npy.gz'))  # These many slices for either GR or F4n1.
+    gr_all = ['dummy'] * total_slices
+    fr_all = ['dummy'] * total_slices
 
     counter = 0
     for run in ['Run1', 'Run2', 'Run62']:  # Assumes three runs with the given names are only present.
         gr_npy_gz_file = f'{BASE_PATH}/example_GR_L128Np256Ng512_{run}_0157_den.npy.gz'
         g = gzip.GzipFile(gr_npy_gz_file, 'r')
-        gr = np.load(g)
         fr_npy_gz_file = f'{BASE_PATH}/example_F4n1_L128Np256Ng512_{run}_0157_den.npy.gz'
         f = gzip.GzipFile(fr_npy_gz_file, 'r')
-        fr = np.load(f)
 
         for i in range(0, Ng):
-            gr_arr_slice = gr[:, :, i]
-            f4n1_arr_slice = fr[:, :, i]
-            gr_all[counter] = gr_arr_slice
-            fr_all[counter] = f4n1_arr_slice
+            gr_all[counter] = g.name + f'_z_{i}'
+            fr_all[counter] = f.name + f'_z_{i}'
 
             counter += 1
 
-            gr_arr_slice = gr[:, i, :]
-            f4n1_arr_slice = fr[:, i, :]
-            gr_all[counter] = gr_arr_slice
-            fr_all[counter] = f4n1_arr_slice
+            gr_all[counter] = g.name + f'_y_{i}'
+            fr_all[counter] = f.name + f'_y_{i}'
 
             counter += 1
 
-            gr_arr_slice = gr[i, :, :]
-            f4n1_arr_slice = fr[i, :, :]
-            gr_all[counter] = gr_arr_slice
-            fr_all[counter] = f4n1_arr_slice
+            gr_all[counter] = g.name + f'_x_{i}'
+            fr_all[counter] = f.name + f'_x_{i}'
 
             counter += 1
 
-    assert gr_all.shape == (total_slices, Ng, Ng)
-    assert fr_all.shape == (total_slices, Ng, Ng)
+    assert len(gr_all) == total_slices
+    assert len(fr_all) == total_slices
+    assert 'dummy' not in gr_all
+    assert 'dummy' not in fr_all
 
-    gr_all, fr_all = unison_shuffled_copies(gr_all, fr_all)
-    rows = gr_all.shape[0]
+    # print(gr_all)
+
+    gr_all, fr_all = unison_shuffled_copies(np.array(gr_all), np.array(fr_all))
+    rows = len(gr_all)
 
     # Perform 80-10-10% split for train-val-test.
-    train_indices = range(np.round(0.8 * rows))
+    train_indices = range(int(np.round(0.8 * rows)))
     val_index_start = train_indices[-1] + 1
-    num_val = np.round(0.1 * rows)
-    val_indices = rows[val_index_start:val_index_start+num_val]
+    num_val = int(np.round(0.1 * rows))
+    val_index_end = val_index_start+num_val
+    val_indices = range(rows)[val_index_start:val_index_end]
     assert len(val_indices) == num_val
     test_index_start = val_indices[-1] + 1
-    num_test = np.round(0.1 * rows)
-    test_indices = rows[test_index_start:test_index_start+num_test]
+    num_test = int(np.round(0.1 * rows))
+    test_index_end = test_index_start+num_test
+    test_indices = range(rows)[test_index_start:test_index_end]
 
-    assert set(train_indices).isdisjoin(set(val_indices))
-    assert set(train_indices).isdisjoin(set(test_indices))
-    assert set(val_indices).isdisjoin(set(test_indices))
+    assert set(train_indices).isdisjoint(set(val_indices))
+    assert set(train_indices).isdisjoint(set(test_indices))
+    assert set(val_indices).isdisjoint(set(test_indices))
 
     # Save train
     for i, index in enumerate(train_indices):
         # Save GR
-        np.save(os.path.join(BASE_PATH, 'gr', 'train', f'{i}.npy'), gr_all[i])
+        np.save(os.path.join(BASE_PATH, 'gr', 'train', f'{i}.npy'), get_slice(gr_all, i))
         # Save FR
-        np.save(os.path.join(BASE_PATH, 'fr', 'train', f'{i}.npy'), fr_all[i])
+        np.save(os.path.join(BASE_PATH, 'fr', 'train', f'{i}.npy'), get_slice(fr_all, i))
 
     # Save val
     for i, index in enumerate(val_indices):
         # Save GR
-        np.save(os.path.join(BASE_PATH, 'gr', 'val', f'{i}.npy'), gr_all[i])
+        np.save(os.path.join(BASE_PATH, 'gr', 'val', f'{i}.npy'), get_slice(gr_all, i))
         # Save FR
-        np.save(os.path.join(BASE_PATH, 'fr', 'val', f'{i}.npy'), fr_all[i])
+        np.save(os.path.join(BASE_PATH, 'fr', 'val', f'{i}.npy'), get_slice(fr_all, i))
 
     # Save test
     for i, index in enumerate(test_indices):
         # Save GR
-        np.save(os.path.join(BASE_PATH, 'gr', 'test', f'{i}.npy'), gr_all[i])
+        np.save(os.path.join(BASE_PATH, 'gr', 'test', f'{i}.npy'), get_slice(gr_all, i))
         # Save FR
-        np.save(os.path.join(BASE_PATH, 'fr', 'test', f'{i}.npy'), fr_all[i])
+        np.save(os.path.join(BASE_PATH, 'fr', 'test', f'{i}.npy'), get_slice(fr_all, i))
