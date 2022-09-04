7b8eda2cf5bb7b513e8497faf8924276d9a451b7
Yash Gondhalekar <yashgondhalekar567@gmail.com>
2022-09-04 17:29:20 +0530

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -32,8 +32,8 @@ if __name__ == "__main__":
 
     if os.path.exists(os.path.join(BASE_PATH, 'gr')):
         shutil.rmtree(os.path.join(BASE_PATH, 'gr'))
-    if os.path.exists(os.path.join(BASE_PATH, 'f4')):
-        shutil.rmtree(os.path.join(BASE_PATH, 'f4'))
+    if os.path.exists(os.path.join(BASE_PATH, 'fr')):
+        shutil.rmtree(os.path.join(BASE_PATH, 'fr'))
  
     os.mkdir(os.path.join(BASE_PATH, 'gr'))
     os.mkdir(os.path.join(BASE_PATH, 'fr'))
@@ -47,75 +47,73 @@ if __name__ == "__main__":
     Ng = 512  # Grid size.
     assert len(glob.glob(f'{BASE_PATH}/example_GR_*_*den.npy.gz')) == len(glob.glob(f'{BASE_PATH}/example_F4n1_*_*den.npy.gz'))
     total_slices = Ng * 3 * len(glob.glob(f'{BASE_PATH}/example_GR_*_*_0157_den.npy.gz'))  # These many slices for either GR or F4n1.
-    gr_all = ['dummy'] * total_slices
-    fr_all = ['dummy'] * total_slices
-
-    counter = 0
-    for run in ['Run1', 'Run2', 'Run62']:  # Assumes three runs with the given names are only present.
-        gr_npy_gz_file = f'{BASE_PATH}/example_GR_L128Np256Ng512_{run}_0157_den.npy.gz'
-        g = gzip.GzipFile(gr_npy_gz_file, 'r')
-        fr_npy_gz_file = f'{BASE_PATH}/example_F4n1_L128Np256Ng512_{run}_0157_den.npy.gz'
-        f = gzip.GzipFile(fr_npy_gz_file, 'r')
-
-        for i in range(0, Ng):
-            gr_all[counter] = g.name + f'_z_{i}'
-            fr_all[counter] = f.name + f'_z_{i}'
-
-            counter += 1
-
-            gr_all[counter] = g.name + f'_y_{i}'
-            fr_all[counter] = f.name + f'_y_{i}'
-
-            counter += 1
-
-            gr_all[counter] = g.name + f'_x_{i}'
-            fr_all[counter] = f.name + f'_x_{i}'
-
-            counter += 1
-
-    assert len(gr_all) == total_slices
-    assert len(fr_all) == total_slices
-    assert 'dummy' not in gr_all
-    assert 'dummy' not in fr_all
-
-    # print(gr_all)
-
-    gr_all, fr_all = unison_shuffled_copies(np.array(gr_all), np.array(fr_all))
-    rows = len(gr_all)
 
     # Perform 80-10-10% split for train-val-test.
-    train_indices = range(int(np.round(0.8 * rows)))
+    train_indices = range(int(np.round(0.8 * total_slices)))
     val_index_start = train_indices[-1] + 1
-    num_val = int(np.round(0.1 * rows))
+    num_val = int(np.round(0.1 * total_slices))
     val_index_end = val_index_start+num_val
-    val_indices = range(rows)[val_index_start:val_index_end]
+    val_indices = range(total_slices)[val_index_start:val_index_end]
     assert len(val_indices) == num_val
     test_index_start = val_indices[-1] + 1
-    num_test = int(np.round(0.1 * rows))
+    num_test = int(np.round(0.1 * total_slices))
     test_index_end = test_index_start+num_test
-    test_indices = range(rows)[test_index_start:test_index_end]
+    test_indices = range(total_slices)[test_index_start:test_index_end]
 
     assert set(train_indices).isdisjoint(set(val_indices))
     assert set(train_indices).isdisjoint(set(test_indices))
     assert set(val_indices).isdisjoint(set(test_indices))
+    assert len(train_indices) + len(val_indices) + len(test_indices) == total_slices
+    
+    counter = 0
+    for run in ['Run1', 'Run2', 'Run62']:  # Assumes three runs with the given names are only present.
+        gr_npy_gz_file = f'{BASE_PATH}/example_GR_L128Np256Ng512_{run}_0157_den.npy.gz'
+        g = gzip.GzipFile(gr_npy_gz_file, 'r')
+        gr = np.load(g)
+        fr_npy_gz_file = f'{BASE_PATH}/example_F4n1_L128Np256Ng512_{run}_0157_den.npy.gz'
+        f = gzip.GzipFile(fr_npy_gz_file, 'r')
+        fr = np.load(f)
 
-    # Save train
-    for i, index in enumerate(train_indices):
-        # Save GR
-        np.save(os.path.join(BASE_PATH, 'gr', 'train', f'{i}.npy'), get_slice(gr_all, i))
-        # Save FR
-        np.save(os.path.join(BASE_PATH, 'fr', 'train', f'{i}.npy'), get_slice(fr_all, i))
+        for i in range(0, Ng):
+            if counter in train_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'train', f'{run}_{counter}.npy'), gr[:, :, i])
+                np.save(os.path.join(BASE_PATH, 'fr', 'train', f'{counter}.npy'), fr[:, :, i])
+            elif counter in val_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'val', f'{run}_{counter}.npy'), gr[:, :, i])
+                np.save(os.path.join(BASE_PATH, 'fr', 'val', f'{counter}.npy'), fr[:, :, i])
+            elif counter in test_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'test', f'{run}_{counter}.npy'), gr[:, :, i])
+                np.save(os.path.join(BASE_PATH, 'fr', 'test', f'{run}_{counter}.npy'), fr[:, :, i])
 
-    # Save val
-    for i, index in enumerate(val_indices):
-        # Save GR
-        np.save(os.path.join(BASE_PATH, 'gr', 'val', f'{i}.npy'), get_slice(gr_all, i))
-        # Save FR
-        np.save(os.path.join(BASE_PATH, 'fr', 'val', f'{i}.npy'), get_slice(fr_all, i))
+            counter += 1
 
-    # Save test
-    for i, index in enumerate(test_indices):
-        # Save GR
-        np.save(os.path.join(BASE_PATH, 'gr', 'test', f'{i}.npy'), get_slice(gr_all, i))
-        # Save FR
-        np.save(os.path.join(BASE_PATH, 'fr', 'test', f'{i}.npy'), get_slice(fr_all, i))
+            if counter in train_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'train', f'{run}_{counter}.npy'), gr[:, i, :])
+                np.save(os.path.join(BASE_PATH, 'fr', 'train', f'{run}_{counter}.npy'), fr[:, i, :])
+            elif counter in val_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'val', f'{run}_{counter}.npy'), gr[:, i, :])
+                np.save(os.path.join(BASE_PATH, 'fr', 'val', f'{run}_{counter}.npy'), fr[:, i, :])
+            elif counter in test_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'test', f'{run}_{counter}.npy'), gr[:, i, :])
+                np.save(os.path.join(BASE_PATH, 'fr', 'test', f'{run}_{counter}.npy'), fr[:, i, :])
+
+            counter += 1
+
+            if counter in train_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'train', f'{run}_{counter}.npy'), gr[i, :, :])
+                np.save(os.path.join(BASE_PATH, 'fr', 'train', f'{run}_{counter}.npy'), fr[i, :, :])
+            elif counter in val_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'val', f'{run}_{counter}.npy'), gr[i, :, :])
+                np.save(os.path.join(BASE_PATH, 'fr', 'val', f'{run}_{counter}.npy'), fr[i, :, :])
+            elif counter in test_indices:
+                np.save(os.path.join(BASE_PATH, 'gr', 'test', f'{run}_{counter}.npy'), gr[i, :, :])
+                np.save(os.path.join(BASE_PATH, 'fr', 'test', f'{run}_{counter}.npy'), fr[i, :, :])
+
+            counter += 1
+
+    print(len([name for name in os.listdir(os.path.join(BASE_PATH, 'gr', 'train')) if os.path.isfile(os.path.join(os.path.join(BASE_PATH, 'gr', 'train'), name))]))
+    print(len([name for name in os.listdir(os.path.join(BASE_PATH, 'gr', 'val')) if os.path.isfile(os.path.join(os.path.join(BASE_PATH, 'gr', 'val'), name))]))
+    print(len([name for name in os.listdir(os.path.join(BASE_PATH, 'gr', 'test')) if os.path.isfile(os.path.join(os.path.join(BASE_PATH, 'gr', 'test'), name))]))
+    print(len([name for name in os.listdir(os.path.join(BASE_PATH, 'fr', 'train')) if os.path.isfile(os.path.join(os.path.join(BASE_PATH, 'fr', 'train'), name))]))
+    print(len([name for name in os.listdir(os.path.join(BASE_PATH, 'fr', 'val')) if os.path.isfile(os.path.join(os.path.join(BASE_PATH, 'fr', 'val'), name))]))
+    print(len([name for name in os.listdir(os.path.join(BASE_PATH, 'fr', 'test')) if os.path.isfile(os.path.join(os.path.join(BASE_PATH, 'fr', 'test'), name))]))
