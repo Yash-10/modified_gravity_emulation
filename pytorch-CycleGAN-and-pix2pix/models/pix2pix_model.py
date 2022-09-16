01ca776b105d8482ec1e58fc27afd316ae9f41ca
Yash-10 <yashgondhalekar567@gmail.com>
2022-09-16 12:36:51 +0530

    Modify weighted L1 implementation

--- pytorch-CycleGAN-and-pix2pix/models/pix2pix_model.py
+++ pytorch-CycleGAN-and-pix2pix/models/pix2pix_model.py
@@ -132,7 +132,6 @@ class Pix2PixModel(BaseModel):
             median_weight = torch.quantile(weight, 0.5)
             weight[weight > median_weight] = 1.
             weight[weight <= median_weight] = 0.5
-            print(torch.unique(weight))
             self.loss_G_L1 = self.criterionL1(weight, self.fake_B, self.real_B) * self.opt.lambda_L1
         else:
             self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
