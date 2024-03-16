1535b30904ab89200272de7a55052436c984f695
Yash-10 <yashgondhalekar567@gmail.com>
2024-03-16 18:05:33 +0530

    Update

--- ml_mg_trial_bicycle.sh
+++ ml_mg_trial_bicycle.sh
@@ -25,11 +25,12 @@ pip install -r requirements.txt
 # Run the program
 pwd
 cd /cosma5/data/durham/dc-gond1/pix2pix_modified_gravity/BicycleGAN
-ls /cosma5/data/durham/dc-gond1/official_pix2pix_data_F4n1_GR_256X256/train | wc -l
-ls /cosma5/data/durham/dc-gond1/official_pix2pix_data_F4n1_GR_256X256/val | wc -l
-ls /cosma5/data/durham/dc-gond1/official_pix2pix_data_F4n1_GR_256X256/test | wc -l
+ls /cosma5/data/durham/dc-gond1/official_pix2pix_data_velDiv_F4n1_GR_256X256/train | wc -l
+ls /cosma5/data/durham/dc-gond1/official_pix2pix_data_velDiv_F4n1_GR_256X256/val | wc -l
+ls /cosma5/data/durham/dc-gond1/official_pix2pix_data_velDiv_F4n1_GR_256X256/test | wc -l
 
-python3 train.py --dataset_mode aligned --dataroot /cosma5/data/durham/dc-gond1/official_pix2pix_data_F4n1_GR_256X256 --name CHECK_F4_256X256 --model bicycle_gan --direction AtoB --batch_size 2 --input_nc 1 --output_nc 1 --netG unet_256 --netD basic_256_multi --netD2 basic_256_multi --load_size 256 --crop_size 256 --num_threads=4 --norm instance --display_id 0 --lr 2e-4 --save_latest_freq 800000 --save_epoch_freq 5 --preprocess none --ndf 128 --ngf 128 --nef 128 --display_freq 10000000 --phase train --netE resnet_256 --gan_mode lsgan --nz 128 --lr_policy step --lr_decay_iters 25 --niter 0 --niter_decay 75 --where_add all --use_dropout --lambda_L1 20.0 --lambda_GAN 2.0
+python3 train.py --dataset_mode aligned --dataroot /cosma5/data/durham/dc-gond1/official_pix2pix_data_velDiv_F4n1_GR_256X256 --name CHECK_F4_veldiv_256X256_scale_minus1600_900 --model bicycle_gan --direction AtoB --batch_size 2 --input_nc 1 --output_nc 1 --netG unet_256 --netD basic_256_multi --netD2 basic_256_multi --load_size 256 --crop_size 256 --num_threads=4 --norm instance --display_id 0 --lr 2e-4 --save_latest_freq 800000 --save_epoch_freq 5 --preprocess none --ndf 128 --ngf 128 --nef 128 --display_freq 10000000 --phase train --netE resnet_256 --gan_mode lsgan --nz 128 --lr_policy step --lr_decay_iters 25 --niter 0 --niter_decay 75 --where_add all --use_dropout --lambda_L1 20.0 --lambda_GAN 2.0
 
 # --netD2 basic_512_multi
 # See https://github.com/junyanz/BicycleGAN/issues/29#issuecomment-402540477
+
--- testing.sh
+++ testing.sh
@@ -6,8 +6,8 @@
 #SBATCH -e testing_standard_error_file.%J.err
 #SBATCH -p cosma8-shm
 #SBATCH -A durham
-#SBATCH -t 24:00:00
-#SBATCH --exclude=mad06,mad05
+#SBATCH -t 02:00:00
+#SBATCH --exclude=mad04,mad05
 #SBATCH --mail-type=END
 #SBATCH --mail-user=f20190481@goa.bits-pilani.ac.in
 
@@ -18,7 +18,15 @@ module load python/3.8.7-C8
 #python3 -u testing_for_den_F4den_validation.py
 #python3 -u testing_for_den_F5den_validation.py
 #python3 -u testing_for_den_F6den_validation.py
-python3 -u testing_for_den_F4den_test.py
+#python3 -u testing_for_den_F4den_test.py
 #python3 -u testing_for_den_F5den_test.py
 #python3 -u testing_for_den_F6den_test.py
+#python3 -u testing_for_den_F4den_validation_Bicycle.py
+#python3 -u testing_for_veldiv_F4den_validation_Bicycle.py
+#python3 -u testing_for_den_F4den_test_Bicycle.py
+python3 -u testing_for_den_F4veldiv_test_Bicycle.py
+
+#python3 -u testing_for_den_F4den_validation_Bicycle_latent_interp.py
+#python3 -u testing_for_den_F4den_test_Bicycle_latent_interp.py
+#python3 -u testing_for_den_F4veldiv_test_Bicycle_latent_interp.py
 
